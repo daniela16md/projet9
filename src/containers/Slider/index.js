9d988add5030f7a0f240d684004c448ce2e49eea
Daniela <danamattl@yahoo.com>
2024-10-02 20:31:35 +0200

    l'odrer d'affichage photos slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,12 +8,12 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? 1 : -1
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
+      () => setIndex((index + 1) % byDateDesc.length),
+      5000,
     );
   };
   useEffect(() => {
